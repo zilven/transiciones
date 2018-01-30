1f6cabc64222c0ba55e9c3227c9112af818a5d33
Maestro <Maestro@imac-de-maestro-2.local>
2018-01-29 17:56:58 -0700

    asdf

--- transiciones/ViewController.swift
+++ transiciones/ViewController.swift
@@ -10,6 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func doSwipDownContenedor(_ sender: Any) {
+        UIView.transition(with: vwContenedorNumero, duration: 0.5, options: [.transitionCurlDown], animations: {
+            self.contador -= 1
+            self.lblNumero.text = String(self.contador)
+        }, completion: nil)
+    }
+    
+    @IBAction func doSwipUpContenedor(_ sender: Any) {
+        UIView.transition(with: vwContenedorNumero, duration: 0.5, options: [.transitionCurlUp], animations: {
+            self.contador += 1
+            self.lblNumero.text = String(self.contador)
+        }, completion: nil)
+    }
+    @IBOutlet weak var vwContenedorNumero: UIView!
+    @IBOutlet weak var lblNumero: UILabel!
+    var contador = 0
+    @IBAction func doTapTransicion(_ sender: Any) {
+        
+        UIView.transition(with: vwContenedorNumero, duration: 0.5, options: [.transitionCurlUp], animations: {
+            self.contador += 1
+            self.lblNumero.text = String(self.contador)
+        }, completion: nil)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
